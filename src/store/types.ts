353f28140334d8cf17818bd6d8c8dbff6ce8ce9d
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-28 19:00:00 +0530

    remove unused file

--- src/store/types.ts
+++
@@ -1,7 +0,0 @@
-import { Mutations } from "./index";
-export type AugmentedActionContext = {
-  commit<K extends keyof Mutations>(
-    key: K,
-    payload: Parameters<Mutations[K]>[1],
-  ): ReturnType<Mutations[K]>;
-} & Omit<ActionContext<State, State>, "commit">;
