1ecf900116efb490f48e27c511f45a4a085f0d37
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-28 15:18:44 +0530

    Header Nav code reorganisation

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app-container">
+    <header-nav />
+    <router-view />
   </div>
-  <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+//#region Imports
+import { defineComponent } from "vue";
+import HeaderNav from "@/components/header-nav.vue";
+//#endregion
 
-#nav {
-  padding: 30px;
+export default defineComponent({
+  name: "app-container",
+  components: {
+    HeaderNav,
+  },
+});
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style lang="scss" scoped>
+.app-container {
 }
 </style>
---
+++ src/components/header-nav.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="component-header-nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+//#region Imports
+import { defineComponent, reactive, toRefs } from "vue";
+//#endregion
+
+export default defineComponent({
+  name: "component-header-nav",
+  setup() {
+    //#region Reactive References
+    const state = reactive({});
+    //#endregion
+
+    //#region Watchers
+    //#endregion
+
+    //#region Lifecycle hooks
+    //#endregion
+
+    //#region Methods
+    //#endregion
+
+    return {
+      ...toRefs(state),
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.component-header-nav {
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
